1fe214e2cd82dae8ef0ba4cfa0a6b0fb10cd4bca
mboerwinkle <mboerwinkle@gmail.com>
2015-12-27 21:15:36 +0000

    made empty test suite function

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "globals.h"
 point3d *pointList;
-int main(){
+extern void testSuite();
+int main(int argc, char *argv[]){
+	if(argc == 2 && 0 == strcmp("-t", argv[1])){
+		testSuite();
+		return 0;
+	}
 	srandom(time(NULL));
 	genPoints();
 	calcCenter();	
