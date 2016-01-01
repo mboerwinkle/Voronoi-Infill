667cef4428c1f9e56bf77675fa9c3722fd160fcc
mboerwinkle <mboerwinkle@gmail.com>
2016-01-01 19:43:52 +0000

    something weird going on with genNodes... I might go ahead and try to integrate it into calcCenter...?

---
+++ genNodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "globals.h"
+node **nodeList;
+void genNodes(){
+	node* thisnode, *testnode;
+	vertex* thisvertex;
+	nodeList = calloc(maxz, sizeof(node*));
+	int temp, test;
+	for(int layer = 0; layer < maxz; layer++){
+		nodeList[layer] = (node*)calloc(vertexCount[layer], sizeof(node));
+		for(temp = 0; temp < vertexCount[layer]; temp++){
+			thisnode = &nodeList[layer][temp];
+			thisvertex = &vertexList[layer][temp];
+			thisnode->loc[0] = thisvertex->loc[0];
+			thisnode->loc[1] = thisvertex->loc[1];
+			for(test = temp+1; test < vertexCount[layer]; test++){
+				if(getSharedParentCount(vertexList[layer][test], *thisvertex) == 2){
+					testnode = &nodeList[layer][test];
+					if(thisnode->sibCount == 3 || testnode->sibCount == 3){
+						puts("too far");
+						continue;
+					}
+					thisnode->sibs[thisnode->sibCount] = testnode;
+					testnode->sibs[testnode->sibCount] = thisnode;
+					thisnode->sibCount++;
+					testnode->sibCount++;
+				}
+			}
+		}
+	}
+}
